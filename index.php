8fcf6a84a01f03f66172102cb4c6bf9b1ce3792f
Andy Minyety <78392694+andyminyety@users.noreply.github.com>
2021-03-19 08:44:04 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,207 @@
+<?php
+    
+    $array1 = array(["numero"=>1,"simbolo"=>"H","nombre"=>"Hidrógeno","tipo"=>"no-metales"],
+    ["numero"=>2,"simbolo"=>"He","nombre"=>"Helio","tipo"=>"gasesnobles"]);
+
+    $array2 = array (["numero"=>3,"simbolo"=>"Li","nombre"=>"Litio","tipo"=>"alcalinos"],
+    ["numero"=>4,"simbolo"=>"Be","nombre"=>"Berilio","tipo"=>"alcalinosterreos"]);
+
+    $array3 = array (["numero"=>5,"simbolo"=>"B","nombre"=>"Boro","tipo"=>"metaloides"],
+    ["numero"=>6,"simbolo"=>"C","nombre"=>"Carbono","tipo"=>"no-metales"],
+    ["numero"=>7,"simbolo"=>"N","nombre"=>"Nitrógeno","tipo"=>"no-metales"],
+    ["numero"=>8,"simbolo"=>"O","nombre"=>"Oxígeno","tipo"=>"no-metales"],
+    ["numero"=>9,"simbolo"=>"F","nombre"=>"Fluor","tipo"=>"halogenos"],
+    ["numero"=>10,"simbolo"=>"Ne","nombre"=>"Neón","tipo"=>"gasesnobles"]);
+
+    $array4 = array (["numero"=>11,"simbolo"=>"Na","nombre"=>"Sodio","tipo"=>"alcalinos"],
+    ["numero"=>12,"simbolo"=>"Mg","nombre"=>"Magnesio","tipo"=>"alcalinosterreos"]);
+
+    $array5 = array (["numero"=>13,"simbolo"=>"Al","nombre"=>"Aluminio","tipo"=>"otrosmetales"],
+    ["numero"=>14,"simbolo"=>"Si","nombre"=>"Silicio","tipo"=>"metaloides"],
+    ["numero"=>15,"simbolo"=>"P","nombre"=>"Fósforo","tipo"=>"no-metales"],
+    ["numero"=>16,"simbolo"=>"S","nombre"=>"Azufre","tipo"=>"no-metales"],
+    ["numero"=>17,"simbolo"=>"Cl","nombre"=>"Cloro","tipo"=>"halogenos"],
+    ["numero"=>18,"simbolo"=>"Ar","nombre"=>"Argón","tipo"=>"gasesnobles"]);
+    
+    $array6 = array (["numero"=>19,"simbolo"=>"K","nombre"=>"Potasio","tipo"=>"alcalinos"],
+    ["numero"=>20,"simbolo"=>"Ca","nombre"=>"Calcio","tipo"=>"alcalinosterreos"],
+    ["numero"=>21,"simbolo"=>"Sc","nombre"=>"Escandio","tipo"=>"metales"],
+    ["numero"=>22,"simbolo"=>"Ti","nombre"=>"Titanio","tipo"=>"metales"],
+    ["numero"=>23,"simbolo"=>"V","nombre"=>"Vanadio","tipo"=>"metales"],
+    ["numero"=>24,"simbolo"=>"Cr","nombre"=>"Cromo","tipo"=>"metales"],
+    ["numero"=>25,"simbolo"=>"Mn","nombre"=>"Manganeso","tipo"=>"metales"],
+    ["numero"=>26,"simbolo"=>"Fe","nombre"=>"Hierro","tipo"=>"metales"],
+    ["numero"=>27,"simbolo"=>"Co","nombre"=>"Cobaldo","tipo"=>"metales"],
+    ["numero"=>28,"simbolo"=>"Ni","nombre"=>"Niquel","tipo"=>"metales"],
+    ["numero"=>29,"simbolo"=>"Cu","nombre"=>"Cobre","tipo"=>"metales"],
+    ["numero"=>30,"simbolo"=>"Zn","nombre"=>"Zinc","tipo"=>"metales"],
+    ["numero"=>31,"simbolo"=>"Ga","nombre"=>"Galio","tipo"=>"otrosmetales"],
+    ["numero"=>32,"simbolo"=>"Ge","nombre"=>"Germanio","tipo"=>"metaloides"],
+    ["numero"=>33,"simbolo"=>"As","nombre"=>"Arsénico","tipo"=>"metaloides"],
+    ["numero"=>34,"simbolo"=>"Se","nombre"=>"Selenio","tipo"=>"no-metales"],
+    ["numero"=>35,"simbolo"=>"Br","nombre"=>"Bromo","tipo"=>"halogenos"],
+    ["numero"=>36,"simbolo"=>"Kr","nombre"=>"Kryptón","tipo"=>"gasesnobles"]);
+
+    $array7 = array (["numero"=>37,"simbolo"=>"Rb","nombre"=>"Rubidio","tipo"=>"alcalinos"],
+    ["numero"=>38,"simbolo"=>"Sr","nombre"=>"Estroncio","tipo"=>"alcalinosterreos"],
+    ["numero"=>39,"simbolo"=>"Y","nombre"=>"Itrio","tipo"=>"metales"],
+    ["numero"=>40,"simbolo"=>"Zr","nombre"=>"Circonio","tipo"=>"metales"],
+    ["numero"=>41,"simbolo"=>"Nb","nombre"=>"Niobio","tipo"=>"metales"],
+    ["numero"=>42,"simbolo"=>"Mo","nombre"=>"Molibdeno","tipo"=>"metales"],
+    ["numero"=>43,"simbolo"=>"Tc","nombre"=>"Tecnecio","tipo"=>"metales"],
+    ["numero"=>44,"simbolo"=>"Ru","nombre"=>"Rutenio","tipo"=>"metales"],
+    ["numero"=>45,"simbolo"=>"Rh","nombre"=>"Rodio","tipo"=>"metales"],
+    ["numero"=>46,"simbolo"=>"Pd","nombre"=>"Paladio","tipo"=>"metales"],
+    ["numero"=>47,"simbolo"=>"Ag","nombre"=>"Plata","tipo"=>"metales"],
+    ["numero"=>48,"simbolo"=>"Cd","nombre"=>"Cadmio","tipo"=>"metales"],
+    ["numero"=>49,"simbolo"=>"In","nombre"=>"Indio","tipo"=>"otrosmetales"],
+    ["numero"=>50,"simbolo"=>"Sn","nombre"=>"Estaño","tipo"=>"otrosmetales"],
+    ["numero"=>51,"simbolo"=>"Sb","nombre"=>"Antimonio","tipo"=>"metaloides"],
+    ["numero"=>52,"simbolo"=>"Te","nombre"=>"Telurio","tipo"=>"metaloides"],
+    ["numero"=>53,"simbolo"=>"I","nombre"=>"Yodo","tipo"=>"halogenos"],
+    ["numero"=>54,"simbolo"=>"Xe","nombre"=>"Xenón","tipo"=>"gasesnobles"]);
+
+    $array8 = array (["numero"=>55,"simbolo"=>"Cs","nombre"=>"Cecio","tipo"=>"alcalinos"],
+    ["numero"=>56,"simbolo"=>"Ba","nombre"=>"Bario","tipo"=>"alcalinosterreos"],
+    ["numero"=>"57-71","simbolo"=>"La-Lu","nombre"=>"Lantanidos","tipo"=>"lantanidos"],
+    ["numero"=>72,"simbolo"=>"Hf","nombre"=>"Hafnio","tipo"=>"metales"],
+    ["numero"=>73,"simbolo"=>"Ta","nombre"=>"Tantalio","tipo"=>"metales"],
+    ["numero"=>74,"simbolo"=>"W","nombre"=>"Wolframio","tipo"=>"metales"],
+    ["numero"=>75,"simbolo"=>"Re","nombre"=>"Renio","tipo"=>"metales"],
+    ["numero"=>76,"simbolo"=>"Os","nombre"=>"Osmio","tipo"=>"metales"],
+    ["numero"=>77,"simbolo"=>"Ir","nombre"=>"Iridio","tipo"=>"metales"],
+    ["numero"=>78,"simbolo"=>"Pt","nombre"=>"Platinio","tipo"=>"metales"],
+    ["numero"=>79,"simbolo"=>"Au","nombre"=>"Oro","tipo"=>"metales"],
+    ["numero"=>80,"simbolo"=>"Hg","nombre"=>"Mercurio","tipo"=>"metales"],
+    ["numero"=>81,"simbolo"=>"Tl","nombre"=>"Talio","tipo"=>"otrosmetales"],
+    ["numero"=>82,"simbolo"=>"Pb","nombre"=>"Plomo","tipo"=>"otrosmetales"],
+    ["numero"=>83,"simbolo"=>"Bi","nombre"=>"Bismuto","tipo"=>"otrosmetales"],
+    ["numero"=>84,"simbolo"=>"Po","nombre"=>"Polonio","tipo"=>"metaloides"],
+    ["numero"=>85,"simbolo"=>"At","nombre"=>"Ástato","tipo"=>"halogenos"],
+    ["numero"=>86,"simbolo"=>"Rn","nombre"=>"Radón","tipo"=>"gasesnobles"]);
+
+    $array9 = array (["numero"=>87,"simbolo"=>"Fr","nombre"=>"Francio","tipo"=>"alcalinos"],
+    ["numero"=>88,"simbolo"=>"Ra","nombre"=>"Radio","tipo"=>"alcalinosterreos"],
+    ["numero"=>"89-103","simbolo"=>"Ac-Lr","nombre"=>"Actinidos","tipo"=>"actinidos"],
+    ["numero"=>104,"simbolo"=>"Rf","nombre"=>"Rutherfordio","tipo"=>"metales"],
+    ["numero"=>105,"simbolo"=>"Db","nombre"=>"Dubnio","tipo"=>"metales"],
+    ["numero"=>106,"simbolo"=>"Sg","nombre"=>"Seaborgio","tipo"=>"metales"],
+    ["numero"=>107,"simbolo"=>"Bh","nombre"=>"Bohrio","tipo"=>"metales"],
+    ["numero"=>108,"simbolo"=>"Hs","nombre"=>"Hassio","tipo"=>"metales"],
+    ["numero"=>109,"simbolo"=>"Mt","nombre"=>"Meitnerio","tipo"=>"metales"],
+    ["numero"=>110,"simbolo"=>"Ds","nombre"=>"Darmstadio","tipo"=>"metales"],
+    ["numero"=>111,"simbolo"=>"Rg","nombre"=>"Roentgenium","tipo"=>"metales"],
+    ["numero"=>112,"simbolo"=>"Cn","nombre"=>"Copernicium","tipo"=>"metales"],
+    ["numero"=>113,"simbolo"=>"Nh","nombre"=>"Nihonium","tipo"=>"otrosmetales"],
+    ["numero"=>114,"simbolo"=>"Fl","nombre"=>"Flerovium","tipo"=>"otrosmetales"],
+    ["numero"=>115,"simbolo"=>"Mc","nombre"=>"Moscovium","tipo"=>"otrosmetales"],
+    ["numero"=>116,"simbolo"=>"Lv","nombre"=>"Livermorium","tipo"=>"otrosmetales"],
+    ["numero"=>117,"simbolo"=>"Ts","nombre"=>"Tennessine","tipo"=>"halogenos"],
+    ["numero"=>118,"simbolo"=>"Og","nombre"=>"Oganesson","tipo"=>"gasesnobles"]);
+
+    $array10 = array (["numero"=>57,"simbolo"=>"La","nombre"=>"Lantano","tipo"=>"lantanidos"],
+    ["numero"=>58,"simbolo"=>"Ce","nombre"=>"Cerio","tipo"=>"lantanidos"],
+    ["numero"=>59,"simbolo"=>"Pr","nombre"=>"Praseodimio","tipo"=>"lantanidos"],
+    ["numero"=>60,"simbolo"=>"Nd","nombre"=>"Neodimio","tipo"=>"lantanidos"],
+    ["numero"=>61,"simbolo"=>"Pm","nombre"=>"Promecio","tipo"=>"lantanidos"],
+    ["numero"=>62,"simbolo"=>"Sm","nombre"=>"Samario","tipo"=>"lantanidos"],
+    ["numero"=>63,"simbolo"=>"Eu","nombre"=>"Europio","tipo"=>"lantanidos"],
+    ["numero"=>64,"simbolo"=>"Gd","nombre"=>"Gadolinio","tipo"=>"lantanidos"],
+    ["numero"=>65,"simbolo"=>"Tb","nombre"=>"Terbio","tipo"=>"lantanidos"],
+    ["numero"=>66,"simbolo"=>"Dy","nombre"=>"Disprosio","tipo"=>"lantanidos"],
+    ["numero"=>67,"simbolo"=>"Ho","nombre"=>"Holmio","tipo"=>"lantanidos"],
+    ["numero"=>68,"simbolo"=>"Er","nombre"=>"Erbio","tipo"=>"lantanidos"],
+    ["numero"=>69,"simbolo"=>"Tm","nombre"=>"Tulio","tipo"=>"lantanidos"],
+    ["numero"=>70,"simbolo"=>"Yb","nombre"=>"Iterbio","tipo"=>"lantanidos"],
+    ["numero"=>71,"simbolo"=>"Lu","nombre"=>"Lutecio","tipo"=>"lantanidos"]);
+
+    $array11 = array (["numero"=>89,"simbolo"=>"Ac","nombre"=>"Actinio","tipo"=>"actinidos"],
+    ["numero"=>90,"simbolo"=>"Th","nombre"=>"Torio","tipo"=>"actinidos"],
+    ["numero"=>91,"simbolo"=>"Pa","nombre"=>"Protactinio","tipo"=>"actinidos"],
+    ["numero"=>92,"simbolo"=>"U","nombre"=>"Uranio","tipo"=>"actinidos"],
+    ["numero"=>93,"simbolo"=>"Np","nombre"=>"Neptunio","tipo"=>"actinidos"],
+    ["numero"=>94,"simbolo"=>"Pu","nombre"=>"Plutonio","tipo"=>"actinidos"],
+    ["numero"=>95,"simbolo"=>"Am","nombre"=>"Americio","tipo"=>"actinidos"],
+    ["numero"=>96,"simbolo"=>"Cm","nombre"=>"Curio","tipo"=>"actinidos"],
+    ["numero"=>97,"simbolo"=>"Bk","nombre"=>"Berkelio","tipo"=>"actinidos"],
+    ["numero"=>98,"simbolo"=>"Cf","nombre"=>"Californio","tipo"=>"actinidos"],
+    ["numero"=>99,"simbolo"=>"Es","nombre"=>"Einstenio","tipo"=>"actinidos"],
+    ["numero"=>100,"simbolo"=>"Fm","nombre"=>"Fermio","tipo"=>"actinidos"],
+    ["numero"=>101,"simbolo"=>"Md","nombre"=>"Mendelevio","tipo"=>"actinidos"],
+    ["numero"=>102,"simbolo"=>"No","nombre"=>"Nobelio","tipo"=>"actinidos"],
+    ["numero"=>103,"simbolo"=>"Lr","nombre"=>"Lawrencio","tipo"=>"actinidos"]);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla Periódica</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body class="arriba">
+    <h1 class="titulo">Tabla Periódica de los Elementos Químicos</h1>
+    <table class="table abajo margin-left margin-bottom-5"">
+        <tr>
+        <?php foreach($array1 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <td colspan="16"></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <?php foreach($array2 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+            <td colspan="10"></td>
+            <?php foreach($array3 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <?php foreach($array4 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+            <td colspan="10"></td>
+            <?php foreach($array5 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?>
+        </tr>
+        <tr>
+        <?php foreach($array6 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <?php foreach($array7 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <?php foreach($array8 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <?php foreach($array9 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+        <tr>
+        <td colspan="3"></td>  
+        <?php foreach($array10 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+        <tr>
+        <td colspan="3"></td>
+        <?php foreach($array11 as $tabla):?> 
+            <td class="td zoom <?php echo $tabla["tipo"];?>"><?php echo $tabla["numero"];?><h1><?php echo $tabla["simbolo"];?></h1><?php echo $tabla["nombre"];?></td>
+            <?php endforeach;?> 
+        </tr>
+    </table>
+</body>
+</html>
